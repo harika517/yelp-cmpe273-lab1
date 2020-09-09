7258a2d74ddee42aec7a49d5c6a258437661c72f
harika517 <hharika38@gmail.com>
2020-09-08 23:37:04 -0700

    Database connection

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const express = require('express');
+const mysql = require('mysql');
 
 const app = express();
 
 app.use(express.json({ extended: false }));
 
 const PORT = process.env.PORT || 3000;
+
+var mysqlConnection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'yelp',
+    multipleStatements: true,
+});
+
+mysqlConnection.connect((err) => {
+    if (!err) {
+        console.log('Database Connected');
+    } else {
+        console.log('Database Connection Failed');
+    }
+});
 
 app.listen(PORT, () => {
     console.log('Server listening to port ' + PORT);
