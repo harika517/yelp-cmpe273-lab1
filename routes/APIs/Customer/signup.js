f2d4285d25b24f96aae2089479d8c1258dc97ea8
harika517 <hharika38@gmail.com>
2020-09-09 19:16:53 -0700

    Customer Registration Successfull

--- routes/APIs/Customer/signup.js
+++ routes/APIs/Customer/signup.js
@@ -15,13 +15,51 @@ router.post(
         ).isLength({ min: 6 }),
     ],
     async(req, res) => {
-        // const { Cust_Name, Cust_email_id, Cust_Password } = req.body;
         // console.log(req.body);
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
-        res.send('Customer Route');
+
+        const { Cust_Name, Cust_email_id, Cust_Password } = req.body;
+        // See if user exists
+        try {
+            mysqlConnectionPool.query(
+                `SELECT Cust_email_id FROM Customer_Information WHERE Cust_email_id = '${Cust_email_id}'`,
+                async(error, result) => {
+                    if (error) {
+                        console.log(error);
+                        return res.status(500).send('Server Error');
+                    }
+                    if (result.length > 0) {
+                        return res
+                            .status(400)
+                            .json({ errors: [{ msg: 'Customer Already Exists' }] });
+                    }
+
+                    //Encrypt password using bcrypt
+                    const salt = await bcrypt.genSalt(10);
+                    const passwordEncrypted = await bcrypt.hash(Cust_Password, salt);
+                    mysqlConnectionPool.query(
+                        `INSERT into Customer_Information (Cust_Name, Cust_email_id, Cust_Password) 
+                        VALUES ('${Cust_Name}', '${Cust_email_id}', '${passwordEncrypted}')`,
+                        (error, result) => {
+                            if (error) {
+                                console.log(error);
+                                return res.status(500).send('Server Error');
+                            }
+                            res.send('Customer Registered');
+                        }
+                    );
+                }
+            );
+            //res.send('Customer Registered');
+        } catch (error) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
+
+        //return jsonwebtoken
     }
 );
 
