d2a2f6a8bba95f4e584eaa1b3d48265199ab403d
harika517 <hharika38@gmail.com>
2020-09-09 17:29:19 -0700

    Validation on signup form success

--- routes/APIs/Customer/signup.js
+++ routes/APIs/Customer/signup.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const mysqlConnectionPool = require('/Users/harika_pradeep/Downloads/CMPE273_Fall2020/lab1/config/connectiondbpool');
 
-router.post('/', async(req, res) => {
-    const { Cust_Name, Cust_email_id, Cust_Password } = req.body;
-    console.log(req.body);
-    res.send('Customer Route');
-});
+router.post(
+    '/', [
+        check('Cust_Name', 'Name is required').not().isEmpty(),
+        check('Cust_email_id', 'Please include valid email').isEmail(),
+        check(
+            'Cust_Password',
+            'Please enter a password with 6 or more characters'
+        ).isLength({ min: 6 }),
+    ],
+    async(req, res) => {
+        // const { Cust_Name, Cust_email_id, Cust_Password } = req.body;
+        // console.log(req.body);
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        res.send('Customer Route');
+    }
+);
 
 module.exports = router;
+
+//Cust_Name:
