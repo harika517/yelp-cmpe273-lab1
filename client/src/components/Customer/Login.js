484df75685fc606c0b91b130ec5cc0892c77fce6
harika517 <hharika38@gmail.com>
2020-09-21 11:27:55 -0700

    Forntend Login page

--- client/src/components/Customer/Login.js
+++ client/src/components/Customer/Login.js
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+  const { email, password } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  return (
+    <Fragment>
+      <h1 className="large text-primary">Login</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Login yelp
+      </p>
+      <form className="form">
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="6"
+            value={password}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Register" />
+      </form>
+      <p className="my-1">
+        Dont have yelp account? <Link to="/signup">SignUp</Link>
+      </p>
+    </Fragment>
+  );
 };
 
 export default Login;
