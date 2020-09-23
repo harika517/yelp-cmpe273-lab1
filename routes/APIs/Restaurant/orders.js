066fb0a2a2fa3bc88d78d9230f9b9c6664cb1310
harika517 <hharika38@gmail.com>
2020-09-23 11:30:43 -0700

    orders backend done

--- routes/APIs/Restaurant/orders.js
+++ routes/APIs/Restaurant/orders.js
@@ -6,7 +6,7 @@ const jwt = require('jsonwebtoken');
 const config = require('config');
 const mysqlConnectionPool = require('../../../config/connectiondbpool');
 
-//@route  GET (View orders raised by customer) '/customer/orders'
+//@route  GET (View orders raised by customer) '/restaurant/orders'
 //@desc   Viewing orders by customer for particular restaurant Id.
 //@access  Private
 //Table Restaurant_Orders
@@ -17,6 +17,35 @@ router.get('/:Rest_Name', (req, res) => {
     try {
         mysqlConnectionPool.query(
             `SELECT * FROM Restaurant_Orders WHERE Rest_Name='${Rest_Name}'`,
+            (error, result) => {
+                if (error) {
+                    console.log(error);
+                    return res.status(500).send('Server Error');
+                }
+                if (result.length === 0) {
+                    return res.status(400).json({
+                        errors: [{ msg: 'No orders for this restaurent' }],
+                    });
+                }
+                console.log(result);
+                res.status(200).json({ result });
+            }
+        );
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Server Error');
+    }
+});
+
+//@route  GET (View orders by status) '/restaurant/orders'
+//@desc   Viewing orders of particular status by restaurant owner.
+//@access  Private
+//Table Restaurant_Orders
+router.get('/:order_status', (req, res) => {
+    const order_status = req.params.order_status;
+    try {
+        mysqlConnectionPool.query(
+            `SELECT * FROM Restaurant_Orders WHERE order_status='${order_status}'`,
             (error, result) => {
                 if (error) {
                     console.log(error);
